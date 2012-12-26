de489f365850d1a09e776145a4e3252ed16cffcf
bingjian <bingjian@e8abccee-9ed2-11dd-af58-c1d6d257e395>
2012-12-26 22:05:12 +0000

    add a few simple tests for decision stump.

--- trunk/abstract_classifier.py
+++ trunk/abstract_classifier.py
@@ -3,25 +3,28 @@
 
 import numpy
 
+def measure_accuracy(Y, o, threshold=0):
+  oo = o.copy()
+  oo[numpy.where(o>threshold)[0]] = 1
+  oo[numpy.where(o<threshold)[0]] = -1
+  d = oo - Y
+  return len(d[numpy.where(d==0)[0]])*1.0/len(Y)
 
 class Classifier(object):
-    def __init__(self):
-        self.verbose = False
-        pass
+  def __init__(self):
+    self.verbose = False
+    pass
 
-    def set_training_sample(self,X,Y,w=None):
-        self.X = X
-        self.Y = Y
-        if w:
-            self.weights = w
-        else:
-            self.set_uniform_weights()
+  def set_training_sample(self,X,Y,w=None):
+    self.X = X
+    self.Y = Y
+    if w:
+      self.weights = w
+    else:
+      self.set_uniform_weights()
 
-    def set_uniform_weights(self):
-        """ Set all examples to have equal uniform weights. """
-        N = len(self.Y)
-        weights = (1.0/N)*numpy.ones(N)
-        self.weights = weights
-
-
-
+  def set_uniform_weights(self):
+    """ Set all examples to have equal uniform weights. """
+    N = len(self.Y)
+    weights = (1.0/N)*numpy.ones(N)
+    self.weights = weights
